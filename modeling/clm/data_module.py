1550c51b0c61b0614f9cdaa17c406cfc249f3d42
p208p2002@gmail.com <p208p2002@gmail.com>
2021-02-07 01:48:34 +0000

    eqg data

--- modeling/clm/data_module.py
+++ modeling/clm/data_module.py
@@ -29,31 +29,7 @@ class DataModule(pl.LightningDataModule):
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=1, shuffle=False)
 
-class RaceDataset(Dataset):
-    def __init__(self,split_set,level,dataset_dir='datasets/RACE',no_label=False):
-        super().__init__()
-        assert split_set in ['dev','test','train']
-        assert level in ['all','middle','high']
-        self.all_file_paths = []
-        for root, dirs, files in os.walk(os.path.join(dataset_dir,split_set)):
-            for f in files:
-                if level == 'all':
-                    self.all_file_paths.append(os.path.join(root,f))
-                elif root == os.path.join(dataset_dir,split_set,level):
-                    self.all_file_paths.append(os.path.join(root,f))
-        #
-        print(split_set,level,dataset_dir,len(self))
-
-        #
-        self.tokenizer = get_tokenizer()
-        self.sep_token = self.tokenizer.sep_token
-        self.pad_token_id = self.tokenizer.pad_token_id
-        self.max_length = 1024
-        self.max_context_length = 850
-        self.no_label = no_label
-
-        # print(self.pad_token_id)
-    
+class UtilsMixin():
     def _prepare_input(self,context,label=None):
         tokenizer = self.tokenizer
 
@@ -93,6 +69,29 @@ class RaceDataset(Dataset):
             model_input[key] = torch.LongTensor(model_input[key])
         
         return model_input
+
+class RaceDataset(Dataset,UtilsMixin):
+    def __init__(self,split_set,level,dataset_dir='datasets/RACE',no_label=False):
+        super().__init__()
+        assert split_set in ['dev','test','train']
+        assert level in ['all','middle','high']
+        self.all_file_paths = []
+        for root, dirs, files in os.walk(os.path.join(dataset_dir,split_set)):
+            for f in files:
+                if level == 'all':
+                    self.all_file_paths.append(os.path.join(root,f))
+                elif root == os.path.join(dataset_dir,split_set,level):
+                    self.all_file_paths.append(os.path.join(root,f))
+        #
+        print(split_set,level,dataset_dir,len(self))
+
+        # config
+        self.tokenizer = get_tokenizer()
+        self.sep_token = self.tokenizer.sep_token
+        self.pad_token_id = self.tokenizer.pad_token_id
+        self.max_length = 1024
+        self.max_context_length = 850
+        self.no_label = no_label    
             
     def __getitem__(self,index):
         with open(self.all_file_paths[index],'r',encoding='utf-8') as f:
@@ -116,3 +115,33 @@ class RaceDataset(Dataset):
             
     def __len__(self):
         return len(self.all_file_paths)
+
+class EQGRaceDataset(Dataset,UtilsMixin):
+    def __init__(self,split_set,level,dataset_dir='datasets/merge-race',no_label=False):
+        self.file_path  = os.path.join(dataset_dir,split_set,level,level+'.jsonl')
+        self.data_lines = open(self.file_path,'r',encoding='utf-8').readlines()
+
+        # config
+        self.tokenizer = get_tokenizer()
+        self.sep_token = self.tokenizer.sep_token
+        self.pad_token_id = self.tokenizer.pad_token_id
+        self.max_length = 1024
+        self.max_context_length = 850
+        self.no_label = no_label
+
+    def __getitem__(self,index):
+        data = json.loads(self.data_lines[index])
+        context = data['article']
+        questions = data['acticle_spec_questions']        
+        questions.append(self.tokenizer.eos_token)
+        label = self.sep_token.join(questions) 
+
+        if not self.no_label:
+            model_input = self._prepare_input(context + self.tokenizer.bos_token, label= label)
+            return model_input['input_ids'],model_input['attention_mask'],model_input['labels']
+        else:
+            model_input = self._prepare_input(context + self.tokenizer.bos_token, label= None)
+            return model_input['input_ids'],model_input['attention_mask']
+    
+    def __len__(self):
+        return len(self.data_lines)
