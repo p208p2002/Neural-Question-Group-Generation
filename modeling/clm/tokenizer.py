24fa9c484923b3a010a45cea358ed0c807b7569a
p208p2002@gmail.com <p208p2002@gmail.com>
2021-02-12 08:58:12 +0000

    add special token if needed

--- modeling/clm/tokenizer.py
+++ modeling/clm/tokenizer.py
@@ -10,7 +10,12 @@ RACE_BOS = _MIDDLE_LEVEL
 
 def get_tokenizer(args = get_args()):
     tokenizer = AutoTokenizer.from_pretrained(args.base_model)
-    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-    tokenizer.add_special_tokens({'sep_token': '[SEP]'})    
+    # add special token if needed
+    if tokenizer.pad_token is None:
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    if tokenizer.sep_token is None:
+        tokenizer.add_special_tokens({'sep_token': '[SEP]'})
+    if tokenizer.eos_token is None:
+        tokenizer.add_special_tokens({'eos_token': '[EOS]'})
     tokenizer.add_tokens([_GENERAL_LEVEL,_EASY_LEVEL,_MIDDLE_LEVEL,_HIGH_LEVEL],special_tokens=True)
     return tokenizer
