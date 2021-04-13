a2607ae9ff412fc1f4c553b796bc48b9149655de
p208p2002 <p208p2002@gmaillcom>
2021-04-13 07:22:01 +0000

    change default

--- utils/argparser.py
+++ utils/argparser.py
@@ -10,7 +10,7 @@ def create_general_parser():
     parser.add_argument('--pick_n',default=5,type=int)
     parser.add_argument('--qgg_optims',default=['ga','first-n','random','greedy'], choices=['ga','first-n','random','greedy'], nargs='+',required=False)
     parser.add_argument('--epoch',default=10,type=int)
-    parser.add_argument('--batch_size',default=8,type=int)
+    parser.add_argument('--batch_size',default=4,type=int)
     parser.add_argument('--lr',type=float,default=5e-6)
     parser.add_argument('--dev',type=int,default=0)
     parser.add_argument('--run_test',action='store_true')
