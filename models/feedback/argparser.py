69b22f11395a725508a19ec3a4a446d11d9c82b6
p208p2002 <p208p2002@gmaillcom>
2021-04-26 01:28:39 +0000

    no message

--- models/feedback/argparser.py
+++ models/feedback/argparser.py
@@ -4,7 +4,7 @@ from utils.argparser import create_base_parser
 def get_args():
     parser = create_base_parser()
     parser.add_argument('-dnl','--disable_negative_loss',action='store_true')
-    parser.add_argument('--alpha',type=float,default=0.05) # 0.3
-    parser.add_argument('--beta',type=float,default=0.75) # 0.4
+    parser.add_argument('--alpha',type=float,default=0.05) # 0.4
+    parser.add_argument('--beta',type=float,default=0.75) # 0.3
     args = parser.parse_args()
     return args
